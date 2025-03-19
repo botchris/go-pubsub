73d7a4eb708ede0749ec879f1d7f37d67e5d791a
jaume.bret <jaume.bret@tangelogames.com>
2025-03-19 12:59:42 +0100

    Update health check URLs in wait-sns.sh and wait-sqs.sh for localstack

--- scripts/wait-sqs.sh
+++ scripts/wait-sqs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-echo "Waiting for SQS at address http://localhost:4566/health, attempting every 5s"
-until $(curl --silent --fail http://localhost:4566/health | grep "\"sqs\": \"running\"" > /dev/null); do
+echo "Waiting for SQS at address http://localhost:4566/_localstack/health, attempting every 5s"
+until $(curl --silent --fail http://localhost:4566/_localstack/health | grep "\"sqs\": \"running\"" > /dev/null); do
     printf '.'
     sleep 5
 done
