b3a09c3e974a9f7064c3031c0521a7545266f596
jaume.bret <jaume.bret@tangelogames.com>
2025-03-19 16:18:25 +0100

    Update health check scripts to verify service availability in localstack

--- scripts/wait-sns.sh
+++ scripts/wait-sns.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo "Waiting for SNS at address http://localhost:4566/_localstack/health, attempting every 5s"
-curl http://localhost:4566/_localstack/health
-until $(curl --silent --fail http://localhost:4566/_localstack/health | grep "\"sns\": \"running\"" > /dev/null); do
+until $(curl --silent --fail http://localhost:4566/_localstack/health | grep "\"sns\": \"available\"" > /dev/null); do
     printf '.'
     sleep 5
 done
