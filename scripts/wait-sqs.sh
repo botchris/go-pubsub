2f82b259677cee0a3f8ac1009082c254b6052862
jaume.bret <jaume.bret@tangelogames.com>
2025-03-19 13:08:19 +0100

    Add health check curl command to wait-sns.sh and wait-sqs.sh

--- scripts/wait-sqs.sh
+++ scripts/wait-sqs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Waiting for SQS at address http://localhost:4566/_localstack/health, attempting every 5s"
+curl --silent --fail http://localhost:4566/_localstack/health
 until $(curl --silent --fail http://localhost:4566/_localstack/health | grep "\"sqs\": \"running\"" > /dev/null); do
     printf '.'
     sleep 5
