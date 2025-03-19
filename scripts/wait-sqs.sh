bc663502d834d3bd50400c5d8f52e43453cf24c0
jaume.bret <jaume.bret@tangelogames.com>
2025-03-19 13:29:25 +0100

    Update health check URLs in wait-sns.sh and wait-sqs.sh to use localhost Remove go-pubsub_net network from docker-compose.yml

--- scripts/wait-sqs.sh
+++ scripts/wait-sqs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Waiting for SQS at address http://localhost:4566/_localstack/health, attempting every 5s"
-curl http://localstack:4566/_localstack/health
-until $(curl --silent --fail http://localstack:4566/_localstack/health | grep "\"sqs\": \"running\"" > /dev/null); do
+curl http://localhost:4566/_localstack/health
+until $(curl --silent --fail http://localhost:4566/_localstack/health | grep "\"sqs\": \"running\"" > /dev/null); do
     printf '.'
     sleep 5
 done
